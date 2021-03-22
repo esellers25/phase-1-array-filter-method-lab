d5d78cc4c128cb12e47f2b9f3a25b0b23c908c85
esellers25 <erin.e.sellers@gmail.com>
2021-03-21 21:49:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+function findMatching(driverArray, name) {
+    let match = driverArray.filter(function(driver){
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+
+    return match
+}
+
+function fuzzyMatch(driverArray, firstLetters) {
+    let iniMatch = driverArray.filter(function(driver){
+        if (driver.substring(0,2) === firstLetters) {
+            return driver
+        }
+    
+    })
+
+    return iniMatch
+}
+
+function matchName (driverArray, name) {
+    return driverArray.filter(function(driver) {
+        if (driver.name === name) {
+        return driver}
+        }
+    )}
